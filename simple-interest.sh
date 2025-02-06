b5cfef39f5042bbf3e0cdd035d822a6a24792a19
zzarema <159909462+zzarema@users.noreply.github.com>
2025-02-07 00:06:08 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Simple Interest Calculation
+
+# Получаем аргументы
+principal=$1
+rate=$2
+time=$3
+
+# Расчёт простого процента
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+
+# Выводим результат
+echo "The simple interest is: $interest"
 
